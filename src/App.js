72b992d25a194b2c944ed992ab15de3774c49962
Cheetah0723 <iyuki723@gmail.com>
2022-05-01 22:30:34 +0900

    remove introduction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Sidebar from './components/sidebar'
-import Introduction from './components/introduction'
+// import Introduction from './components/introduction'
 import About from './components/about'
 import Timeline from './components/timeline'
 
